e2a6891cf6e2f8077e7b29c5703e368b5a00c3bc
hao难懂 <46086267+haobye@users.noreply.github.com>
2022-12-01 16:20:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
 import os
 import random
+import http.client, urllib
 
 today = datetime.now()
 start_date = os.environ['START_DATE']
